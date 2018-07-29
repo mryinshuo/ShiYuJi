045569c9e6c71889f42f986feaa8eea08cc46203
HBY <socalledhby@gmail.com>
2018-07-29 16:54:54 +0800

    ver 1.0

---
+++ app/src/main/java/com/hby/shiyuji/Main3Activity.java
@@ -0,0 +1,56 @@
+package com.hby.shiyuji;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+public class Main3Activity extends TabActivity {
+
+    private RadioButton bottom1_pindao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhuye);
+
+        findViewById(R.id.bottom1_pindao).setBackgroundResource(R.drawable.pindaodianjihou);
+        findViewById(R.id.bottom1_dongtai).setBackgroundResource(R.drawable.dongtaidianjihou);
+        findViewById(R.id.bottom1_zhuye).setBackgroundResource(R.drawable.zhuyedianjiqian);
+
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec 推荐 = tabHost.newTabSpec("001").setIndicator("推荐").setContent(R.id.page1_tuijian);
+        TabHost.TabSpec 直播 = tabHost.newTabSpec("002").setIndicator("直播").setContent(R.id.page2_zhibo);
+        TabHost.TabSpec 附近 = tabHost.newTabSpec("003").setIndicator("附近").setContent(R.id.page3_fujin);
+        TabHost.TabSpec 其他 = tabHost.newTabSpec("004").setIndicator("其他").setContent(R.id.page4_qita);
+        tabHost.addTab(推荐);
+        tabHost.addTab(直播);
+        tabHost.addTab(附近);
+        tabHost.addTab(其他);
+
+        bottom1_pindao = (RadioButton) findViewById(R.id.bottom1_pindao);
+        bottom1_pindao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main3Activity.this, channel.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void shipinleidianji(View view){
+        Toast.makeText(this,"欢迎浏览视频",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,shipinleiActivity.class);
+        startActivity(intent);
+    }
+    public  void fanhuizhuyemian(View view){
+        Intent intent = new Intent(this,Main3Activity.class);
+        startActivity(intent);
+    }
+
+
+}
+
---
+++ app/src/main/java/com/hby/shiyuji/Write.java
@@ -0,0 +1,76 @@
+package com.hby.shiyuji;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Write extends AppCompatActivity implements View.OnClickListener{
+
+    private View inflate;
+    private TextView choosePhoto;
+    private TextView takePhoto;
+    private Dialog dialog;
+    private Object view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+        //写动态页面左上按钮“取消 ”，返回查看动态页
+        Button like3 = (Button) findViewById(R.id.like3);
+        like3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
+    }
+        public void show(View view){
+            dialog = new Dialog(this,R.style.ActionSheetDialogStyle);
+            //填充对话框的布局
+            inflate = LayoutInflater.from(this).inflate(R.layout.camera_contral, null);
+            //初始化控件
+            choosePhoto = (TextView) inflate.findViewById(R.id.choosePhoto);
+            takePhoto = (TextView) inflate.findViewById(R.id.takePhoto);
+            choosePhoto.setOnClickListener(this);
+            takePhoto.setOnClickListener(this);
+            //将布局设置给Dialog
+            dialog.setContentView(inflate);
+            //获取当前Activity所在的窗体
+            Window dialogWindow = dialog.getWindow();
+            //设置Dialog从窗体底部弹出
+            dialogWindow.setGravity( Gravity.BOTTOM);
+            //获得窗体的属性
+            WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+            lp.y = 20;//设置Dialog距离底部的距离
+//       将属性设置给窗体
+            dialogWindow.setAttributes(lp);
+            dialog.show();//显示对话框
+        }
+
+
+
+//拍照和从相册中选择的监听
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.takePhoto:
+                Toast.makeText(this,"点击了拍照",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.choosePhoto:
+                Toast.makeText(this,"点击了从相册选择",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        dialog.dismiss();
+    }
+}
